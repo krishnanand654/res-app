888d8e0f2a68c44813451f25ebf2b61358d015a9
Krishnanand A <krishnanand654@gmail.com>
2024-03-07 02:54:27 +0530

    keyboard-fix

--- Screens/Chat.jsx
+++ Screens/Chat.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Location from 'expo-location';
-import { View, Text, TextInput, Button, FlatList, StyleSheet, ActivityIndicator, Pressable, KeyboardAvoidingView, Platform } from 'react-native';
+import { View, Text, TextInput, Button, FlatList, StyleSheet, ActivityIndicator, Pressable, KeyboardAvoidingView, Platform, Keyboard } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
 const ChatScreen = () => {
+    const [keyboardVisible, setKeyboardVisible] = useState(false);
     const [messages, setMessages] = useState([]);
     const [inputMessage, setInputMessage] = useState('');
     const [senderName, setSenderName] = useState('');
@@ -15,6 +16,33 @@ const ChatScreen = () => {
     const [loadingLocation, setLoadingLocation] = useState(false);
 
     const navigation = useNavigation();
+
+    useEffect(() => {
+        const keyboardDidShowListener = Keyboard.addListener(
+            'keyboardDidShow',
+            () => {
+                setKeyboardVisible(true);
+            }
+        );
+        const keyboardDidHideListener = Keyboard.addListener(
+            'keyboardDidHide',
+            () => {
+                setKeyboardVisible(false);
+            }
+        );
+
+        return () => {
+            keyboardDidHideListener.remove();
+            keyboardDidShowListener.remove();
+        };
+    }, []);
+
+    const inputContainerStyle = {
+        ...styles.inputContainer,
+        height: keyboardVisible ? 150 : 80, // Adjust height based on keyboard visibility
+    };
+
+
 
     const handleSendLocation = async () => {
         try {
@@ -44,7 +72,6 @@ const ChatScreen = () => {
         }
     };
 
-    console.log(inputMessage);
     useEffect(() => {
         retrieveData();
     }, []);
@@ -66,23 +93,23 @@ const ChatScreen = () => {
 
     const fetchMessages = async () => {
         try {
-            const response = await fetch('http://10.10.10.1/messages');
-            const data = await response.json();
-            setMessages(data.messages);
+            // const response = await fetch('http://10.10.10.1/messages');
+            // const data = await response.json();
+            // setMessages(data.messages);
 
-            // const localMessages = [
-            //     { id: 1, username: 'Kris', message: 'Hello' },
-            //     { id: 2, username: 'Krishnanand', message: 'Hi' },
-            //     { id: 3, username: 'Kris', message: 'How are you?' },
-            //     { id: 4, username: 'Kris', message: 'How are you?' },
-            //     { id: 5, username: 'Krishnanand', message: 'I"m stuck here please help me please' },
-            //     { id: 6, username: 'Krishnanand', message: '9.510057576982812,76.55070881721772' },
-            //     { id: 7, username: 'Kris', message: 'Hi Ay' },
+            const localMessages = [
+                { id: 1, username: 'Kris', message: 'Hello' },
+                { id: 2, username: 'Krishnanand', message: 'Hi' },
+                { id: 3, username: 'Kris', message: 'How are you?' },
+                { id: 4, username: 'Kris', message: 'How are you?' },
+                { id: 5, username: 'Krishnanand', message: 'I"m stuck here please help me please' },
+                { id: 6, username: 'Krishnanand', message: '9.459296075730787, 76.52225665031116' },
+                { id: 7, username: 'Kris', message: 'Hi Ay' },
 
 
-            // ];
+            ];
 
-            // setMessages(localMessages);
+            setMessages(localMessages);
         } catch (error) {
             console.error('No messages');
             console.log('error', error)
@@ -136,7 +163,7 @@ const ChatScreen = () => {
                                 const [latitude, longitude] = item.message.split(',');
                                 handlePressLocation(parseFloat(latitude), parseFloat(longitude));
                             }}>
-                                <Text style={[styles.messageText, item.username === senderName ? styles.senderMessageText : styles.receiverMessageText]}>{item.message} <Feather name="external-link" size={24} style={styles.linkIcon} /></Text>
+                                <Text style={[styles.messageText, item.username === senderName ? styles.senderMessageText : styles.receiverMessageText]}>{item.message} <Feather name="external-link" size={20} style={styles.linkIcon} /></Text>
 
                             </Pressable>
                         ) : (
@@ -148,19 +175,20 @@ const ChatScreen = () => {
             />
 
 
-            <View style={styles.inputContainer}>
+            <View style={inputContainerStyle}>
+                <Pressable style={styles.button} onPress={handleSendLocation}>
+                    <Entypo name="location-pin" size={28} color="#006ee6" />
+                </Pressable>
                 <TextInput
                     style={styles.input}
                     value={inputMessage}
                     onChangeText={(text) => setInputMessage(text)}
-                    placeholder="Type your message..."
+                    placeholder=""
                     multiline
                 />
-                <Pressable style={styles.button} onPress={handleSendLocation}>
-                    <Entypo name="location-pin" size={35} color="#006ee6" />
-                </Pressable>
+
                 <Pressable style={styles.button} onPress={handleSendMessage}>
-                    <Feather name="send" size={32} color="#006ee6" />
+                    <Feather name="send" size={26} color="#006ee6" />
                 </Pressable>
             </View>
             {loadingLocation && (
@@ -236,12 +264,15 @@ const styles = StyleSheet.create({
     },
     inputContainer: {
         backgroundColor: "#eee",
-        paddingTop: 20,
+        paddingTop: 10,
+        paddingBottom: 30,
         flexDirection: 'row',
-        alignItems: 'center',
-        paddingHorizontal: 10,
+
+        paddingHorizontal: 20,
         borderTopWidth: 1,
         borderTopColor: '#ccc',
+
+
     },
     input: {
         fontSize: 18,
@@ -249,9 +280,9 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingHorizontal: 10,
         paddingVertical: 8,
-        marginRight: 10,
+        marginRight: 20,
         borderWidth: 1,
-        height: 50,
+        height: 30,
         borderColor: '#ccc',
         borderRadius: 20,
     },
