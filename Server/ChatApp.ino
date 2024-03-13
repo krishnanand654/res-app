dac64e44bdd0aa0c0c4dbf7fa3ebec36672e3425
Krishnanand A <krishnanand654@gmail.com>
2024-03-14 03:06:05 +0530

    sockets completed

--- Server/ChatApp.ino
+++ Server/ChatApp.ino
@@ -3,149 +3,254 @@
 #include <ESP8266WebServer.h>
 #include "FS.h"
 #include <ArduinoJson.h>
+#include <WebSocketsServer.h>
 
-const byte        DNS_PORT = 53;
-const String      messagesFile = "/messages.txt";
-const char*       wifiName = "ChatWithMe";
+// Configuration
+const byte DNS_PORT = 53;
+const String messagesFile = "/messages.txt";
+const char* wifiName = "ChatWithMe";
+const int webSocketPort = 81;
 
-IPAddress         apIP(10, 10, 10, 1);
-DNSServer         dnsServer;
-ESP8266WebServer  webServer(80);
+// Network
+IPAddress apIP(10, 10, 10, 1);
+DNSServer dnsServer;
+ESP8266WebServer webServer(80);
+WebSocketsServer webSocket = WebSocketsServer(webSocketPort);
+
+// In-memory representation of messages
+String messages;
+
+// Previous modification timestamp of the messages file
+unsigned long prevFileModifiedTime = 0;
 
 void setup() {
   Serial.begin(115200);
   SPIFFS.begin();
 
+  // WiFi setup
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   WiFi.softAP(wifiName);
+
+  // Start DNS server
   dnsServer.start(DNS_PORT, "*", apIP);
-  
+
+  // HTTP server routes
   webServer.on("/", HTTP_GET, []() {
-  webServer.enableCORS(true);// Allow requests from any origin
-  webServer.send(200, "text/html", "Welcome to the ESP8266 File Server");
-});
+    webServer.send(200, "text/html", "Welcome to the ESP8266 WebSocket Server");
+  });
   webServer.on("/send", HTTP_POST, handleSendMessage);
   webServer.on("/messages", HTTP_GET, showMessages);
   webServer.on("/clear", HTTP_POST, handleClearMessages);
 
+  // WebSocket event handler
+  webSocket.begin();
+  webSocket.onEvent(handleWebSocketEvent);
 
+  // Not found handler
   webServer.onNotFound(handleNotFound);
-   webServer.enableCORS(true);
+
+  // Enable CORS
+  webServer.enableCORS(true);
+
+  // Start HTTP server
   webServer.begin();
 }
 
-void handleNotFound()
-{
-    if (webServer.method() == HTTP_OPTIONS)
-    {
-       
-        webServer.sendHeader("Access-Control-Allow-Origin", "*");
-        webServer.sendHeader("Access-Control-Max-Age", "10000");
-        webServer.sendHeader("Access-Control-Allow-Methods", "PUT,POST,GET,OPTIONS");
-        webServer.sendHeader("Access-Control-Allow-Headers", "*");
-        webServer.enableCORS(true);
-        webServer.send(204);
-    }
-    else
-    {
-        webServer.send(404, "text/plain", "");
-    }
+void loop() {
+  dnsServer.processNextRequest();
+  webSocket.loop(); // Handle WebSocket events
+  webServer.handleClient();
+
+  // Check for file changes every 5 seconds
+  static unsigned long prevCheckTime = 0;
+  if (millis() - prevCheckTime > 5000) {
+    prevCheckTime = millis();
+    checkFileChanges();
+  }
 }
 
+void checkFileChanges() {
+  File file = SPIFFS.open(messagesFile, "r");
+  if (file) {
+    unsigned long currentFileModifiedTime = file.getLastWrite();
+    file.close();
 
+    if (currentFileModifiedTime != prevFileModifiedTime) {
+      prevFileModifiedTime = currentFileModifiedTime;
+      updateMessages();
+    }
+  } else {
+    Serial.println("Error opening messages file");
+  }
+}
 
+void updateMessages() {
+  messages = ""; // Clear existing messages
+  File file = SPIFFS.open(messagesFile, "r");
+  if (file) {
+    while (file.available()) {
+      messages += file.readStringUntil('\n');
+    }
+    file.close();
+    broadcastMessages();
+  } else {
+    Serial.println("Error opening messages file");
+  }
+}
+
+void broadcastMessages() {
+  webSocket.broadcastTXT(messages);
+}
+
+void handleWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_TEXT: {
+      // Handle text message
+      String message = String((char*)payload);
+      Serial.print("Received message: ");
+      Serial.println(message);
+
+      String messages;
+      // Read JSON data from messages.txt file
+      File file = SPIFFS.open(messagesFile, "r");
+      if (file) {
+           while (file.available()) {
+          messages += file.readStringUntil('\n');
+          }
+          file.close();
+          String jsonResponse = messages;
+          webServer.send(200, "application/json", jsonResponse);
+          Serial.println("broadcasting");
+          // Broadcast the messages via WebSocket
+          webSocket.broadcastTXT(jsonResponse);
+          
+     
+      } else {
+        Serial.println("Error opening messages file");
+      }
+      break;
+    }
+    default:
+      break;
+  }
+}
 
 void handleSendMessage() {
-  if (webServer.hasArg("message") && webServer.hasArg("username")) {
+  // Handle sending message via HTTP POST
+  if (webServer.hasArg("message") && webServer.hasArg("username") && webServer.hasArg("receiver") && webServer.hasArg("phoneNumber")) {
     String message = webServer.arg("message");
     String username = webServer.arg("username");
-    
+    String receiver = webServer.arg("receiver");
+    String phoneNumber = webServer.arg("phoneNumber");
+
     Serial.print("Received message from ");
     Serial.print(username);
-    Serial.print(": ");
+    Serial.print(" to ");
+    Serial.print(receiver);
+    Serial.print(" (Phone Number: ");
+    Serial.print(phoneNumber);
+    Serial.print("): ");
     Serial.println(message);
+    
+    // Open the JSON file in write mode
+    File file = SPIFFS.open(messagesFile, "a+"); // Open file in append mode or create if not exists
+    if (!file) {
+      Serial.println("Error opening messages file for writing");
+      webServer.send(500, "text/plain", "Error opening messages file for writing");
+      return;
+    }
     
     // Parse the existing JSON data from the file
     DynamicJsonDocument doc(1024);
-    File file = SPIFFS.open(messagesFile, "r");
-    if (file) {
+    if (file.size() == 0) {
+      // If the file is empty, initialize with an empty array
+      doc = DynamicJsonDocument(1024);
+      JsonArray messages = doc.createNestedArray("messages");
+    } else {
       DeserializationError error = deserializeJson(doc, file);
-      file.close();
       if (error) {
+        Serial.println("Error parsing existing messages: " + String(error.c_str()));
+        file.close();
         webServer.send(500, "text/plain", "Error parsing existing messages");
         return;
       }
-    } else {
-      doc = DynamicJsonDocument(1024);
-      JsonArray messages = doc.createNestedArray("messages");
     }
     
     // Create a new JSON object for the new message
     JsonObject newMessage = doc["messages"].createNestedObject();
     newMessage["id"] = doc["messages"].size() + 1; // Increment ID
     newMessage["username"] = username;
+    newMessage["receiver"] = receiver;
+    newMessage["phoneNumber"] = phoneNumber;
     newMessage["message"] = message;
     
-    // Open the JSON file in write mode
-    file = SPIFFS.open(messagesFile, "w");
-    if (file) {
-      // Serialize the JSON document to the file
-      if (serializeJson(doc, file) == 0) {
-        file.close();
-        webServer.send(500, "text/plain", "Error saving message");
-        return;
-      }
-      file.close();
-      
-      // Send response indicating success
-      webServer.enableCORS(true);
-      webServer.send(200, "text/plain", "Message Sent");
-    } else {
-      webServer.send(500, "text/plain", "Error saving message");
+    // Serialize the JSON document to the file
+    file.close();
+    file = SPIFFS.open(messagesFile, "w"); // Re-open file in write mode to truncate
+    if (!file) {
+      Serial.println("Error reopening messages file for writing");
+      webServer.send(500, "text/plain", "Error reopening messages file for writing");
+      return;
     }
+    if (serializeJson(doc, file) == 0) {
+      file.close();
+      Serial.println("Error saving message");
+      webServer.send(500, "text/plain", "Error saving message");
+      return;
+    }
+    file.close();
+
+    // Broadcast the message via WebSocket
+    webServer.enableCORS(true);
+    webServer.send(200, "text/plain", "Message Sent");
+//    broadcastMessages();
   } else {
-    webServer.send(400, "text/plain", "No message or username provided");
+    webServer.send(400, "text/plain", "Missing required parameters (message, username, receiver, phoneNumber)");
   }
-} 
+   updateMessages();
+}
 
 
 void handleClearMessages() {
+  // Handle clearing messages via HTTP POST
   if (SPIFFS.remove(messagesFile)) {
-    webServer.enableCORS(true);// Allow requests from any origin
     webServer.send(200, "text/plain", "Messages Cleared");
   } else {
     webServer.send(500, "text/plain", "Error clearing messages");
   }
+  
 }
 
 void showMessages() {
+  // Handle showing messages via HTTP GET and WebSocket
   String messages;
   File file = SPIFFS.open(messagesFile, "r");
-  
   if (file) {
-    // Read messages from the file and store them in a String
     while (file.available()) {
       messages += file.readStringUntil('\n');
     }
     file.close();
-    
-    // Construct a JSON object containing the messages
     String jsonResponse = messages;
-    
-    // Set the appropriate headers for CORS
-    webServer.enableCORS(true); // Allow requests from any origin
-    webServer.sendHeader("Content-Type", "application/json");
-    
-    // Send the JSON response
     webServer.send(200, "application/json", jsonResponse);
+
+    // Broadcast the messages via WebSocket
+    webSocket.broadcastTXT(jsonResponse);
   } else {
-    // If there's an error reading the messages file, send a 500 error response
     webServer.send(500, "text/plain", "Error reading messages");
   }
 }
 
-void loop() {
-  dnsServer.processNextRequest();
-  webServer.handleClient();
+
+void handleNotFound() {
+  // Handle not found routes
+  if (webServer.method() == HTTP_OPTIONS) {
+    webServer.sendHeader("Access-Control-Allow-Origin", "*");
+    webServer.sendHeader("Access-Control-Max-Age", "10000");
+    webServer.sendHeader("Access-Control-Allow-Methods", "PUT,POST,GET,OPTIONS");
+    webServer.sendHeader("Access-Control-Allow-Headers", "*");
+    webServer.send(204);
+  } else {
+    webServer.send(404, "text/plain", "");
+  }
 }
